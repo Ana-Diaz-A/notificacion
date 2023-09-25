d830fd22fd677d5bfaaa4471be80a78bc27ec4b7
Ana <116079787+Ana-Diaz-A@users.noreply.github.com>
2023-09-25 13:07:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//CONSTANTES
+const notification = document.querySelector('.notification');
+const button = document.querySelector('.trigger-button');
+
+
+//FUNCIÓN
+const toggleNotification = () => {
+    notification.classList.remove('none');
+    notification.classList.toggle('hide');
+}
+
+document.addEventListener("click", (event) => {
+    const isClickInsideNotification = notification.contains(event.target);
+    const isButtonClicked = button.contains(event.target);
+
+    if (!isClickInsideNotification && !isButtonClicked) {
+        notification.classList.add('hide');
+    }
+});
